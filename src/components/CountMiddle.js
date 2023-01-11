a83414ce44f2f342260d22f154b7ebaf6551736d
Ian Okumu <okumu.otsembo@gmail.com>
2023-01-11 12:18:40 +0300

    Completed

--- src/components/CountMiddle.js
+++ src/components/CountMiddle.js
@@ -2,11 +2,18 @@ import React, { useState } from "react";
 
 function CountMiddle(){
 
+    const [count, setCount] = useState(0)
+
+    function clicked(){
+        setCount(count + 1)
+    }
+
     return(
             <div>
                 <center>
-                    <h4 className="btn-text">0</h4>
-                    <button className="timeButton">CLICK ME</button>
+                    <h4 className="btn-text">{count}</h4>
+                    <button className="timeButton"
+                    onClick={() => setCount(count + 1)}>CLICK ME</button>
                 </center>
             </div>
     )
--- src/components/CountUp.js
+++ src/components/CountUp.js
@@ -2,10 +2,16 @@ import React, { useState } from "react";
 
 function CountUp(){
 
+    const [ count, setCount ] = useState(0)
+
+    // setInterval(()=> {
+    //     setCount(count + 1)
+    // }, 1000)
+
     return (
             <div>
                 <center>
-                    <h4 className="time-text">0</h4>
+                    <h4 className="time-text">{count}</h4>
                 </center>
             </div>
     )
